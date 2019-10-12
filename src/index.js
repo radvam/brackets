b2aab907c36075b17d6aa518252b4ddf8ec39519
Dmitry Radkovets <dmitry.radkovets@gmail.com>
2019-10-13 00:10:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let itemStr = str.split('');
+  let stack = [];
+  
+  if (!(itemStr.length % 2 === 0))
+    return false;
+  
+  for (let i = 0; i <= itemStr.length - 1; i++) {
+    for (let j = 0; j <= bracketsConfig.length - 1; j++) {
+
+      if (itemStr[i] === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]) {
+        if (stack[stack.length - 1] === bracketsConfig[j][0]) {
+          stack.pop();
+          break;
+        } else if (stack[stack.length - 1] !== bracketsConfig[j][0]) {
+          stack.push(itemStr[i]);
+          break;
+        } else {
+          return false;
+        }
+      }
+
+      if (itemStr[i] === bracketsConfig[j][0]) {
+        stack.push(itemStr[i]);
+        break;  
+      }
+
+      if (itemStr[i] === bracketsConfig[j][1]) {
+        if(stack[stack.length - 1] === bracketsConfig[j][0]) {
+          stack.pop();
+          break;
+        } else {
+          return false;
+        }
+      }
+
+    }
+  }
+  return !stack.length;
 }
